058f2aef99aad4248e141407a8eaf17ab575e8cf
Ju minhark <wnalsgkr@gmail.com>
2021-06-04 12:32:38 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import { getPosts } from './actions/posts';
@@ -10,6 +10,7 @@ import Form from './components/Form/Form';
 import useStyles from './styles';
 
 const App = () => {
+	const [currentId, setCurrentId] = useState(null);
 	const classes = useStyles();
 	const dispatch = useDispatch();
 
@@ -38,10 +39,10 @@ const App = () => {
 						spacing={2}
 					>
 						<Grid item xs={12} sm={7}>
-							<Posts />
+							<Posts setCurrentId={setCurrentId} />
 						</Grid>
 						<Grid item xs={12} sm={4}>
-							<Form />
+							<Form currentId={currentId} setCurrentId={setCurrentId} />
 						</Grid>
 					</Grid>
 				</Container>
