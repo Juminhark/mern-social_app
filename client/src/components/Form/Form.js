32a0b5578725cdc50d1d2609e98b7cc32aa010a6
Ju minhark <wnalsgkr@gmail.com>
2021-06-07 23:01:52 +0900

    post_remove

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 
-import { useDispatch } from 'react-redux';
-import { createPost } from '../../actions/posts';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { createPost, updatePost } from '../../actions/posts';
 
 import useStyles from './styles';
 
@@ -14,8 +15,15 @@ const Form = ({ currentId, setCurrentId }) => {
 		tags: '',
 		selectedFile: '',
 	});
+	const post = useSelector((state) =>
+		currentId ? state.posts.find((p) => p._id === currentId) : null
+	);
 	const classes = useStyles();
 	const dispatch = useDispatch();
+
+	useEffect(() => {
+		if (post) setPostData(post);
+	}, [post]);
 
 	const addFileToPost = (e) => {
 		let file = e.target.files[0];
@@ -31,10 +39,25 @@ const Form = ({ currentId, setCurrentId }) => {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
-		dispatch(createPost(postData));
+		if (currentId) {
+			dispatch(updatePost(currentId, postData));
+		} else {
+			dispatch(createPost(postData));
+		}
+
+		clear();
 	};
 
-	const clear = () => {};
+	const clear = () => {
+		setCurrentId(null);
+		setPostData({
+			creator: '',
+			title: '',
+			message: '',
+			tags: '',
+			selectedFile: '',
+		});
+	};
 
 	return (
 		<Paper className={classes.paper}>
@@ -44,7 +67,9 @@ const Form = ({ currentId, setCurrentId }) => {
 				className={`${classes.root} ${classes.form}`}
 				onSubmit={handleSubmit}
 			>
-				<Typography variant="h6">Creating a Memory</Typography>
+				<Typography variant="h6">
+					{currentId ? 'Editing' : 'Creating'} a Memory
+				</Typography>
 				<TextField
 					name="creator"
 					variant="outlined"
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -2,20 +2,23 @@ import React from 'react';
 import {
 	Card,
 	CardActions,
-	CarContent,
+	CardContent,
 	CardMedia,
 	Button,
 	Typography,
-	CardContent,
 } from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import useStyles from './styles';
+import { useDispatch } from 'react-redux';
+
+import { deletePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
 	const classes = useStyles();
+	const dispatch = useDispatch();
 
 	return (
 		<Card className={classes.card}>
@@ -44,8 +47,11 @@ const Post = ({ post, setCurrentId }) => {
 					{post.tags.map((tag) => `#${tag} `)}
 				</Typography>
 			</div>
+			<Typography className={classes.title} variant="h5" gutterBottom>
+				{post.title}
+			</Typography>
 			<CardContent>
-				<Typography className={classes.title} variant="h5" gutterBottom>
+				<Typography variant="h5" gutterBottom>
 					{post.message}
 				</Typography>
 			</CardContent>
@@ -55,7 +61,11 @@ const Post = ({ post, setCurrentId }) => {
 					Like
 					{post.likeCount}
 				</Button>
-				<Button size="small" color="primary" onClick={() => {}}>
+				<Button
+					size="small"
+					color="primary"
+					onClick={() => dispatch(deletePost(post._id))}
+				>
 					<DeleteIcon fontSize="small" />
 					Delete
 				</Button>
