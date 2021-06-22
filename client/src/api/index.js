1736f790f18410d57efcf7804e2e7021d81d58de
Ju minhark <wnalsgkr@gmail.com>
2021-06-23 00:27:38 +0900

    deploy

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({
+	baseURL: 'https://juminhark-mern-social-app-server.zeet.app/',
+});
 
 API.interceptors.request.use((req) => {
 	if (localStorage.getItem('profile')) {
