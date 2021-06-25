e0ddf787bf8fc8353e07913460597edd385284d5
Ju minhark <wnalsgkr@gmail.com>
2021-06-25 12:39:46 +0900

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-	baseURL: 'https://juminhark-mern-social-app-server.zeet.app/',
+	baseURL: 'juminhark-mern-social-app-server.zeet.app/',
 });
 
 // Deploy baseURL: 'https://juminhark-mern-social-app-server.zeet.app/'
